55304def4c00fac0bd5231b9e5f27d6b0c7f1748
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-04-15 13:17:40 +0300

    feat: resolve queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -142,8 +142,10 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce((acc, promise) => {
+    return acc.then((result) => promise.then((data) => result + data));
+  }, Promise.resolve(''));
 }
 
 module.exports = {
